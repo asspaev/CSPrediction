6bf00307462f97360f07b90315119c1ed77f2ae9
Nikita Aspaev <asspaev@yandex.ru>
2025-03-11 10:09:19 +0500

    add router api to main app

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ import uvicorn
 
 from contextlib import asynccontextmanager
 
+from api import router
 from core import settings
 from models import (
     db_helper,
@@ -19,12 +20,16 @@ async def lifespan(fast_app: FastAPI):
     print('dispose engine')
     await db_helper.dispose()
 
-
-
 app = FastAPI(
     default_response_class=ORJSONResponse,
     lifespan=lifespan,
 )
+
+app.include_router(
+    router,
+    prefix=settings.api.prefix,
+)
+
 
 if __name__ == "__main__":
     uvicorn.run(
