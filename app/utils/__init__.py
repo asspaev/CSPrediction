e26ee04eef90c4a3e2d4fd29283f8c7173ef5d73
Nikita Aspaev <asspaev@yandex.ru>
2025-05-08 15:25:47 +0500

    Add templates

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -3,9 +3,11 @@ __all__ = (
     "create_access_token",
     "hash_string",
     "Predicator",
+    "templates",
 )
 
 from .case_converter import camel_case_to_snake_case
 from .create_access_token import create_access_token
 from .hash_string import hash_string
 from .predicator import Predicator
+from .templates import templates
